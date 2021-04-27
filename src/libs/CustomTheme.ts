7c0ef0e11b6e487dbc47e7e9a6de845b818845ef
Cesar <contact@cesarb.dev>
2021-04-27 10:52:31 +0200

    upd

--- src/libs/CustomTheme.ts
+++ src/libs/CustomTheme.ts
@@ -1,12 +1,7 @@
 import { extendTheme } from '@chakra-ui/react';
 
 const theme = extendTheme({
-  colors: {
-    brand: {
-      100: '#f7fafc',
-      900: '#1a202c',
-    },
-  },
+  colors: {},
 });
 
 export default theme;
